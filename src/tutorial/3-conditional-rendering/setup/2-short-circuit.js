2ccb2241ee5909d390a8ef8b3c93804893ae07ff
sara-gray <saragrayen@gmail.com>
2020-12-12 12:10:38 +0000

    tenery operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,23 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState('sara');
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>Value : {secondValue}</h1> */}
+      <h1>{text || 'hello world'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        Toggle error
+      </button>
+      {/* {isError && <h2>Error...</h2>} */}
+      {isError ? <h2>Error...</h2> : <h2>No error</h2>}
+    </>
+  );
 };
 
 export default ShortCircuit;
